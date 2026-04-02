9b7e572f792f927d0e09b2fe9cc7c9fa16a634fc
alvaro.franco <alvarojf96@hotmail.com>
2026-04-02 17:54:07 +0100

    fix unit test test_routers

--- tests/unit/infrastructure/test_routers.py
+++ tests/unit/infrastructure/test_routers.py
@@ -4,7 +4,8 @@ from fastapi.testclient import TestClient
 from unittest.mock import Mock
 
 from src.infrastructure.adapters.ingoing.routers import router, get_predict_use_case
-from src.application.dtos.dtos import ForecastResponseDTO
+# 1. Importamos também o HistoricalDataDTO
+from src.application.dtos.dtos import ForecastResponseDTO, HistoricalDataDTO 
 
 app = FastAPI()
 app.include_router(router)
@@ -13,7 +14,13 @@ client = TestClient(app)
 
 def test_predict_endpoint_success():
     mock_use_case = Mock()
-    mock_use_case.execute.return_value = ForecastResponseDTO(predicted_quantity=145.5)
+    
+    mock_history = [HistoricalDataDTO(date="2024-12-24", quantity=140.0)]
+    
+    mock_use_case.execute.return_value = ForecastResponseDTO(
+        predicted_quantity=145.5,
+        history=mock_history
+    )
 
     app.dependency_overrides[get_predict_use_case] = lambda: mock_use_case
 
@@ -26,7 +33,14 @@ def test_predict_endpoint_success():
     response = client.post("/predict", json=payload)
 
     assert response.status_code == 200
-    assert response.json() == {"predicted_quantity": 145.5}
+    
+    assert response.json() == {
+        "predicted_quantity": 145.5,
+        "history": [
+            {"date": "2024-12-24", "quantity": 140.0}
+        ]
+    }
+    
     mock_use_case.execute.assert_called_once()
 
     app.dependency_overrides.clear()
